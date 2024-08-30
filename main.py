90cd831c523f1d1c86d43ab6b936c2f8edb85350
ricebiceps <ricebiceps@gmail.com>
2024-08-30 19:06:49 -0400

    added triangular hitbox for asteroids

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+
         pygame.display.flip()
+        
+        #limit the fps to 60
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
